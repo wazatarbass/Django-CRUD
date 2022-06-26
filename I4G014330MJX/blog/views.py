75177a39269a0d1845b4ccafd64a08fd1a406b20
Adewale Lawal <87212727+wazatarbass@users.noreply.github.com>
2022-06-26 23:26:07 +0100

    second update

--- I4G014330MJX/blog/views.py
+++ I4G014330MJX/blog/views.py
@@ -1,23 +1,30 @@
+from calendar import c
+from typing import List
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from .models import Post
 
-class PostListView(CreateView):
+class PostListView(ListView):
     model = Post
     
 class PostCreateView(CreateView):
     model = Post
-    fields = "__all__"
-    success_url = reverse_lazy(“blog:all”)]
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
     
-class PostDetailView(CreateView):
+class PostDetailView(DetailView):
     model = Post
-        
-class PostUpdateView(CreateView):
+    
+class PostUpdateView(UpdateView):
     model = Post
     fields = “__all__”
     success_url  = reverse_lazy(“blog:all”)
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = “__all__”
+    success_url = reverse_lazy(“blog:all”)
+    
+    
+ 
 
-class PostDeleteView(CreateView):
-    model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
